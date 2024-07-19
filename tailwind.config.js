6f08138d12e3ff10bc74a273e2f3e984c921a824
Prince Oncada <prince.oncada@gmail.com>
2024-07-19 15:38:54 -0400

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: "x-",
+  important: true,
+  content: ["./**/*.{html,js,css,liquid}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
