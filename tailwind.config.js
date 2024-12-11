f018cbd7fa3ebb76586627134b01192611b00396
prince <prince.oncada@gmail.com>
2024-12-11 10:17:44 -0500

    dev: removed daisy-ui and added different screen breakpoints for md and lg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: "x-",
-  important: true,
-  content: ["./**/*.{html,js,css,liquid}"],
-  theme: {
-    extend: {
-        fontFamily: {
-            'nanum': ["'Nanum Myeongjo', serif"],
-            'causten': ["'Causten'"],
+    prefix: "x-",
+    important: true,
+    content: ["./**/*.{html,js,css,liquid}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'nanum': ["'Nanum Myeongjo', serif"],
+                'causten': ["'Causten'"],
+            },
+        },
+        screens: {
+            md: "990px",
+            lg: "1280px",
         },
     },
-  },
-  plugins: [
-    require('daisyui'),
-  ],
+    plugins: [],
 }
